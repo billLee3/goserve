3bba0e5511c11b1c56942f3b37204bbe030100b2
billLee3 <bill.lee.third@gmail.com>
2025-10-31 16:05:39 -0500

    Add stateful counter along with wrapper function

--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
 
+type apiConfig struct {
+	fileserverhits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverhits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	// const logopath = "./image.png"
+	mux := http.NewServeMux()
+
+	apiCfg := apiConfig{fileserverhits: atomic.Int32{}}
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("/healtz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	//Readiness Endpoint
+	// mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+	// 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// 	w.WriteHeader(http.StatusOK)
+	// 	_, err := w.Write([]byte("OK"))
+	// 	if err != nil {
+	// 		fmt.Printf("Error writing response: %v\n", err)
+	// 	}
+	// })
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverhits.Load())))
 }
